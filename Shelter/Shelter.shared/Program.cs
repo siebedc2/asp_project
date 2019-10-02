26fb264fc0bbd337a1d1be40d0ea3169b5bf0812
Sam <samlinck@outlook.com>
2019-10-02 17:19:32 +0200

    first class

--- Shelter/Shelter.shared/Program.cs
+++ Shelter/Shelter.shared/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Shelter.shared
 {
@@ -8,5 +9,11 @@ namespace Shelter.shared
         {
             Console.WriteLine("Hello World!");
         }
+    }
+    
+    public class Shelter {
+        public string Name {get; set;}
+        public ICollection<Employee> Employees {get; set;}
+        public ICollection<Animal> Animals {get; set;}
     }
 }
