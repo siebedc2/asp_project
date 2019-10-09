e482322af8b0e93d6ea087a59ae37f74c46c5b89
Arne Ameye <arne.ameye@outlook.com>
2019-10-09 15:55:28 +0200

    added ShelterController

---
+++ ShelterMvc/Controllers/ShelterController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ShelterMvc.Models;
+
+namespace ShelterMvc.Controllers
+{
+    public class ShelterController : Controller
+    {
+        private readonly ILogger<ShelterController> _logger;
+
+        public ShelterController(ILogger<ShelterController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
