4eb9be375ba1970db5ab9532af1c9e47d7566489
Arne Ameye <arne.ameye@outlook.com>
2019-12-30 12:32:21 +0100

    commit to resolve merge

--- Shelter/Shelter.shared/Employee.cs
+++ Shelter/Shelter.shared/Employee.cs
@@ -8,17 +8,21 @@ namespace Shelter.shared{
     
     {// Begin Employee
         public string Name { get; set; }
+        public int ShelterId { get; set; }
     }
     public class Manager:Employee {
         // Properties manager
+        public bool LicensedManager { get; set; }
     }
 
     public class Caretaker:Employee {
         // Properties caretaker
+        public bool FixedContract { get; set; }
     }
 
     public class Administrator:Employee {
         // Properties administrator
+        public bool DigitalAdministration { get; set; }
     }
     // End Employee
 }
